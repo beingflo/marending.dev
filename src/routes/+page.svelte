aecb5273ec0676fadfc84ac7b39b29913c78caf9
Florian Marending <florian@marending.dev>
2022-11-06 20:16:01 +0100

    Add basic structure of landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<div class="p-4">
-	<h1 class="font-bold text-3xl">florian</h1>
-	<h1 class="font-bold text-3xl pl-8">marending</h1>
+<div class="flex flex-col">
+	<div class="p-4">
+		<h1 class="font-bold text-3xl w-fit">florian</h1>
+		<h1 class="font-bold text-3xl pl-8 -m-3 w-fit">marending</h1>
+	</div>
+</div>
+<div class="absolute bottom-8 right-12 flex flex-row gap-4">
+	<a class="text-md font-semibold" href="/metrics">metrics</a>
+	<a class="text-md font-semibold" href="/about">about</a>
 </div>
