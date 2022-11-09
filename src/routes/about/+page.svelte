1d6c4e691df6a4009dbd7b1b1d607846b4fc4ec1
Florian Marending <florian@marending.dev>
2022-11-09 21:05:18 +0100

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<main class="p-4 md:p-24 grid gap-2 gap-y-8 grid-cols-1 lg:grid-cols-2">
+	<div>
+		<h1 class="font-bold text-2xl pb-4">Hi</h1>
+		<p>My name is Florian Marending, I'm a software engineer.</p>
+		<p>Currently, my interests include lean web development and robust backends.</p>
+	</div>
+	<img src="/portrait.jpeg" alt="portrait" />
+</main>
