6c630fd92f1018ba347adb36b5fcf99b5a50c74b
Florian Marending <florian@marending.dev>
2022-11-10 21:46:51 +0100

    Add header and nav elements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,18 @@
 	import '../inter.css';
 </script>
 
-<div class="flex flex-row justify-between">
+<header class="flex flex-row justify-between">
 	<div class="flex flex-col">
 		<a href="/" class="p-4">
 			<h1 class="font-bold text-2xl md:text-3xl w-fit">florian</h1>
 			<h1 class="font-bold text-2xl md:text-3xl ml-5 -mt-3 w-fit">marending</h1>
 		</a>
 	</div>
-	<div class="flex flex-row gap-6 pr-6 pt-5 md:pt-6">
+	<nav class="flex flex-row gap-6 pr-6 pt-5 md:pt-6">
 		<a class="text-sm md:text-md font-semibold h-fit" href="/metrics">metrics</a>
 		<a class="text-sm md:text-md font-semibold h-fit" href="/about">about</a>
-	</div>
-</div>
+	</nav>
+</header>
 
 <main>
 	<slot />
