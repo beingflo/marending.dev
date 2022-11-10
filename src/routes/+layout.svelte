08a0f70258cea0120cbb081ee457399ef1037f84
Florian Marending <florian@marending.dev>
2022-11-10 21:34:32 +0100

    Clean up margin on name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 	<div class="flex flex-col">
 		<a href="/" class="p-4">
 			<h1 class="font-bold text-2xl md:text-3xl w-fit">florian</h1>
-			<h1 class="font-bold text-2xl md:text-3xl pl-8 -m-3 w-fit">marending</h1>
+			<h1 class="font-bold text-2xl md:text-3xl ml-5 -mt-3 w-fit">marending</h1>
 		</a>
 	</div>
 	<div class="flex flex-row gap-6 pr-6 pt-5 md:pt-6">
