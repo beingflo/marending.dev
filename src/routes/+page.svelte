27946f4c928424e9a310bc4344a7e3cf64616c4f
Florian Marending <florian@marending.dev>
2022-11-11 16:03:17 +0100

    Align posts on landing page better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-8 md:pt-24 gap-8 md:gap-12 px-4 md:justify-items-center"
+	class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-8 md:pt-24 gap-8 md:gap-12 px-4 md:px-12"
 >
 	<NoteSummary
 		title="A study of lines"
