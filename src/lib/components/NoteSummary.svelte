f0f45a10d7e78512b0c1d15449b994158e658e12
Florian Marending <florian@marending.dev>
2022-11-11 16:10:36 +0100

    Attempt removing active state on mobile

--- src/lib/components/NoteSummary.svelte
+++ src/lib/components/NoteSummary.svelte
@@ -5,7 +5,7 @@
 	export let link: string;
 </script>
 
-<a class="w-fit" href={link}>
+<a class="w-fit active:select-none" href={link}>
 	<p class="text-sm md:text-md text-gray-600">{date}</p>
 	<h3 class="text-md md:text-lg font-bold">{title}</h3>
 	<p class="text-sm md:text-md">{summary}</p>
