2efbf3c6e0070989e5fd31a7dce4d99b72050fdf
Florian Marending <florian@marending.dev>
2022-11-12 18:37:42 +0100

    Remove placeholder posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,27 +6,9 @@
 	class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-8 md:pt-24 gap-8 md:gap-12 px-4 md:px-12"
 >
 	<NoteSummary
-		title="A study of lines"
-		summary="Some schmuck tries his hands at generative art"
-		date="06/11/2022"
-		link="/lines"
-	/>
-	<NoteSummary
-		title="Technical internals of this website"
+		title="About this website"
 		summary="There are many like it, but this one is mine"
-		date="12/10/2022"
+		date="12/11/2022"
 		link="/website"
-	/>
-	<NoteSummary
-		title="Building a foosball ranking application"
-		summary="How to waste time at work"
-		date="04/09/2022"
-		link="/foosball"
-	/>
-	<NoteSummary
-		title="A hasslefree server setup for personal projects"
-		summary="Don't spend more time on infra than on your projects"
-		date="10/11/2022"
-		link="/server"
 	/>
 </div>
--- src/routes/website/+page.svelte
+++ src/routes/website/+page.svelte
@@ -7,45 +7,7 @@
 </script>
 
 <Note>
-	<Date>12/10/2022</Date>
-	<Title>Technical internals of this website</Title>
+	<Date>12/11/2022</Date>
+	<Title>About this website</Title>
 	<Subtitle>There are many like it, but this one is mine</Subtitle>
-
-	<P>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend eros quis risus dapibus
-		tincidunt. Nunc tempor mi at commodo rhoncus. Suspendisse placerat ac lacus ac eleifend. Donec
-		egestas lacus risus, vitae dictum nisi venenatis id. Curabitur ligula libero, tincidunt id
-		tempus eget, ultrices sit amet nulla. Fusce quis orci ac nibh viverra vehicula. Duis finibus
-		massa nibh, id vehicula ex tempor ac. Morbi sit amet ipsum vel elit rhoncus ullamcorper. Mauris
-		et purus eget nunc tristique fringilla. Duis fringilla massa id tortor luctus blandit.
-	</P>
-	<P>
-		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-		the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-		of type and scrambled it to make a type specimen book.
-	</P>
-	<P>
-		It has survived not only five centuries, but also the leap into electronic typesetting,
-		remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-		sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-		Aldus PageMaker including versions of Lorem Ipsum.
-	</P>
-	<P>
-		Why do we use it? It is a long established fact that a reader will be distracted by the readable
-		content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-		more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
-		making it look like readable English.
-	</P>
-	<P>
-		Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model
-		text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-		versions have evolved over the years, sometimes by accident, sometimes on purpose (injected
-		humour and the like).
-	</P>
-	<P>
-		Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model
-		text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-		versions have evolved over the years, sometimes by accident, sometimes on purpose (injected
-		humour and the like).
-	</P>
 </Note>
