a33feb9d39d53ee7acbff2836e4f8170597b4465
Florian Marending <florian@marending.dev>
2022-11-20 21:42:43 +0100

    Flesh out feedback admin page

--- src/routes/feedback/+page.svelte
+++ src/routes/feedback/+page.svelte
@@ -1,18 +1,26 @@
 <script>
+	import Note from '$lib/components/Note.svelte';
+
 	export let data;
 	const { feedbacks } = data;
 </script>
 
-{#await feedbacks}
-	Loading
-{:then feedbacks}
-	<div class="p-2 flex flex-col gap-2">
-		{#each feedbacks as feedback}
-			<p>{feedback.content}</p>
-			<p>{feedback.email}</p>
-			<p>{feedback.timestamp}</p>
-		{/each}
-	</div>
-{:catch someError}
-	System error: {someError.message}.
-{/await}
+<Note>
+	{#await feedbacks}
+		Loading
+	{:then feedbacks}
+		<div class="p-2 flex flex-col gap-12">
+			{#each feedbacks as feedback}
+				<div class="flex flex-col">
+					<p class="font-semibold text-sm">{feedback.email || 'unknown'}</p>
+					<p class="text-gray-500 text-sm">
+						{feedback.timestamp}
+					</p>
+					<p>{feedback.content}</p>
+				</div>
+			{/each}
+		</div>
+	{:catch someError}
+		System error: {someError.message}.
+	{/await}
+</Note>
