7264d47436553dd831f685100fec131b0b9f5933
Florian Marending <florian@marending.dev>
2022-11-20 16:33:26 +0100

    Add cursor pointer and change padding of details

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -2,11 +2,11 @@
 	export let title: string;
 </script>
 
-<details class="p-2 mb-3 border border-black bg-gray-50">
-	<summary>
+<details class="mb-3 border border-black bg-gray-50">
+	<summary class="cursor-pointer p-2">
 		{title}
 	</summary>
-	<div class="pt-2">
+	<div class="p-2">
 		<slot />
 	</div>
 </details>
