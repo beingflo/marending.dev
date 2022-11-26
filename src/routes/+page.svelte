d9b60885c071e58f1accdfbd2d594dbac49c4eed
Florian Marending <florian@marending.dev>
2022-11-26 14:24:49 +0100

    Move notes to subdir

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,10 @@
 </script>
 
 <div
-	class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-8 md:pt-24 gap-8 md:gap-12 px-4 md:px-12"
->
+	class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-8 md:pt-24 gap-8 md:gap-12 px-4 md:px-12">
 	<NoteSummary
 		title="About this website"
 		summary="There are many like it, but this one is mine"
 		date="12/11/2022"
-		link="/website"
-	/>
+		link="/notes/website" />
 </div>
