61fa4b2f72aea8847defb3f0182258a3fe380210
Florian Marending <florian@marending.dev>
2022-11-27 21:28:20 +0100

    Put graphs side by side

--- src/routes/metrics/+page.svelte
+++ src/routes/metrics/+page.svelte
@@ -26,15 +26,19 @@
 	};
 </script>
 
-<Note>
-	<h1 class="text-lg">Page views</h1>
-	<div class="">
-		<Chart
-			data={lineChart}
-			type="line"
-			lineOptions={{ spline: 1, hideDots: 1, regionFill: 1 }}
-			axisOptions={{ yAxisMode: 'tick', xAxisMode: 'tick', xIsSeries: 1 }} />
+<div class="p-2 md:p-8 grid grid-cols-1 md:grid-cols-2">
+	<div>
+		<h1 class="text-lg">Page views</h1>
+		<div class="">
+			<Chart
+				data={lineChart}
+				type="line"
+				lineOptions={{ spline: 1, hideDots: 1, regionFill: 1 }}
+				axisOptions={{ yAxisMode: 'tick', xAxisMode: 'tick', xIsSeries: 1 }} />
+		</div>
 	</div>
-	<h1 class="text-lg pt-16">Most viewed</h1>
-	<Chart data={pieChart} type="pie" maxSlices={8} />
-</Note>
+	<div>
+		<h1 class="text-lg pt-8 md:pt-0">Most viewed</h1>
+		<Chart data={pieChart} type="pie" maxSlices={8} />
+	</div>
+</div>
