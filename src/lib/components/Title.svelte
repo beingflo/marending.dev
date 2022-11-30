84f9fc802a32a3fe5328bd0f681aff17c3247922
Florian Marending <florian@marending.dev>
2022-11-30 21:59:55 +0100

    Add more space around title

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <p class="text-gray-600 mb-2">{date}</p>
-<h1 class="text-2xl md:text-4xl mb-1"><slot /></h1>
-<h2 class="text-lg md:text-xl border-b border-b-gray-400 pb-2 mb-8">{subtitle}</h2>
+<h1 class="text-2xl md:text-4xl mb-1 text-black"><slot /></h1>
+<h2 class="text-lg md:text-xl mb-16 md:mb-24">{subtitle}</h2>
 
 <style>
 	h1 {
