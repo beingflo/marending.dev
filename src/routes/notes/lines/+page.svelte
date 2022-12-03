f85f57f4e60904826de508f7298cdfb83aa94a15
Florian Marending <florian@marending.dev>
2022-12-03 22:00:18 +0100

    Continue writing /lines

--- src/routes/notes/lines/+page.svelte
+++ src/routes/notes/lines/+page.svelte
@@ -2,9 +2,27 @@
 	import P from '$lib/components/P.svelte';
 	import Note from '$lib/components/Note.svelte';
 	import Title from '$lib/components/Title.svelte';
+	import A from '$lib/components/A.svelte';
 </script>
 
 <Note>
-	<Title date="03 December 2022" subtitle="Some schmuck tries his hands at generative art"
-		>A study of lines</Title>
+	<Title date="03 December 2022" subtitle="Some schmuck tries his hands at generative art">
+		A study of lines
+	</Title>
+	<P>
+		After moving into a bigger apartement, I've found the walls of our office to be a bit naked.
+		While I'm a hobby photographer and would like to get more into printing my work, for this space
+		I have something else in mind.
+	</P>
+	<P>
+		It started with finding out about <em>generative art</em> via
+		<A href="https://tylerxhobbs.com/fidenza">Tyler Hobbs' Fidenza</A>. I found the look of this
+		type of art very appealing. Maybe because I lack creativity, I like the idea of <em>guiding</em>
+		randomness towards aestethic order, rather than <em>create</em> from scratch.
+	</P>
+	<P>
+		On this page I want to systematically explore techniques and patterns to generate interesting
+		structures. To prevent myself from getting lost in the endless possibilities, I will restrict
+		myself to <span class="hover:line-through decoration-1 font-bold">lines</span>.
+	</P>
 </Note>
