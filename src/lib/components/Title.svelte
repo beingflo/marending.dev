b7ecaee50e3a13de60d53d0cd2d46c90d1a36513
Florian Marending <florian@marending.dev>
2022-12-03 21:15:35 +0100

    Remove special font just for title

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -4,11 +4,5 @@
 </script>
 
 <p class="text-gray-600 mb-2">{date}</p>
-<h1 class="text-2xl md:text-4xl mb-1 text-black"><slot /></h1>
+<h1 class="text-2xl md:text-4xl mb-1 text-black font-extrabold"><slot /></h1>
 <h2 class="text-lg md:text-xl mb-16 md:mb-24">{subtitle}</h2>
-
-<style>
-	h1 {
-		font-family: 'LatoLatinWebBlack';
-	}
-</style>
