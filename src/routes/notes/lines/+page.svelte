2b2157abd8f8fce3bcf492088357b459d48c00e4
Florian Marending <florian@marending.dev>
2022-12-03 22:19:47 +0100

    Write svg background in /lines

--- src/routes/notes/lines/+page.svelte
+++ src/routes/notes/lines/+page.svelte
@@ -3,6 +3,8 @@
 	import Note from '$lib/components/Note.svelte';
 	import Title from '$lib/components/Title.svelte';
 	import A from '$lib/components/A.svelte';
+	import Details from '$lib/components/Details.svelte';
+	import C from '$lib/components/C.svelte';
 </script>
 
 <Note>
@@ -25,4 +27,22 @@
 		structures. To prevent myself from getting lost in the endless possibilities, I will restrict
 		myself to <span class="hover:line-through decoration-1 font-bold">lines</span>.
 	</P>
+	<Details title="How do I go about generating my lines?">
+		Initially I had two programming languages in mind: <C>Rust</C> or <C>Typescript</C>. <C>Rust</C>
+		being my favorite language was an obvious choice. However, the dynamic nature of <C>TS</C> lends
+		itself for a problem like this. No sense in fighting <C>Rusts</C> borrow-checker for interactive
+		code like this, and the upsides you get in turn don't really come into play here. What further seals
+		the deal for me is that <C>TS</C>-based code can be natively embedded right here, on this page.
+		<br />
+		Next, I was exploring libraries that would make the whole ordeal easier. Top contenders include
+		<A href="https://p5js.org/">p5js</A>, <A href="https://ptsjs.org/">Pts.js</A> and <A
+			href="http://paperjs.org/">Paper.js</A
+		>. Ultimately, I came to the conclusion that I don't need the extensive feature sets of any of
+		these libraries. Animation, physics and geometry utilities are nice-to-haves but unlikely to see
+		any use in my use-case. Instead, all I really need is a way to programatically draw lines,
+		ideally in a way that I can get a high-resolution image out of it somehow for print. And what
+		better way to do this than with our good friend, the <C>SVG</C>. To get a slightly nicer
+		interface I use
+		<A href="https://svgjs.dev/docs/3.0/">SVG.js</A>.
+	</Details>
 </Note>
