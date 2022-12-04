5817cf95a3069c586e585caf93f15b8df34411c8
Florian Marending <florian@marending.dev>
2022-12-04 22:27:23 +0100

    Add configuration to Code component

--- src/lib/components/Code.svelte
+++ src/lib/components/Code.svelte
@@ -4,13 +4,14 @@
 	import theme from 'svelte-highlight/styles/stackoverflow-light';
 
 	export let value: string;
+	export let respectMargin: boolean = false;
 </script>
 
 <svelte:head>
 	{@html theme}
 </svelte:head>
 
-<div class="border border-black mb-6 lg:-mx-4">
+<div class="border border-black mb-6 {respectMargin || 'lg:-mx-4'}">
 	<Highlight language={javascript} code={value} />
 </div>
 
