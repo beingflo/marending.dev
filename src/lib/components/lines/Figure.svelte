94515e9d8a3131583451412fd78b38b1e21cc657
Florian Marending <florian@marending.dev>
2022-12-04 22:27:41 +0100

    Give figures more space

--- src/lib/components/lines/Figure.svelte
+++ src/lib/components/lines/Figure.svelte
@@ -2,9 +2,9 @@
 	export let caption = '';
 </script>
 
-<div class="w-fit mx-auto pb-2">
+<div class="w-fit mx-auto pb-6 pt-4">
 	<slot />
 	{#if caption}
-		<div class="pt-3 text-center text-gray-800 text-sm italic">{caption}</div>
+		<div class="pt-4 text-center text-gray-800 text-sm italic">{caption}</div>
 	{/if}
 </div>
