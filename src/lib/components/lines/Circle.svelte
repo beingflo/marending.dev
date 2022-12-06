361f776cdb881f8246aaeafb67414466df2964c7
Florian Marending <florian@marending.dev>
2022-12-06 21:28:39 +0100

    [/lines] Center small svgs

--- src/lib/components/lines/Circle.svelte
+++ src/lib/components/lines/Circle.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <Figure caption="Fig 1. Not a straight line">
-	<svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="80" cy="80" r="50" fill-opacity="0" stroke="black" />
-	</svg>
+	<div class="w-fit mx-auto">
+		<svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+			<circle cx="80" cy="80" r="50" fill-opacity="0" stroke="black" />
+		</svg>
+	</div>
 </Figure>
--- src/lib/components/lines/SimpleLine.svelte
+++ src/lib/components/lines/SimpleLine.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <Figure caption="Fig 2. A straight line">
-	<svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
-		<line x1="40" y1="80" x2="120" y2="80" stroke="black" />
-	</svg>
+	<div class="w-fit mx-auto">
+		<svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+			<line x1="40" y1="80" x2="120" y2="80" stroke="black" />
+		</svg>
+	</div>
 </Figure>
