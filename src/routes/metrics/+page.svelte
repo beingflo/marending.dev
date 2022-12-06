72f30b667681287e90d621105ccd9cbde1bf241d
Florian Marending <florian@marending.dev>
2022-12-06 20:49:35 +0100

    Switch chart library

--- src/routes/metrics/+page.svelte
+++ src/routes/metrics/+page.svelte
@@ -1,43 +1,49 @@
-<script>
-	// @ts-nocheck
-	import Chart from 'svelte-frappe-charts';
+<script lang="ts">
+	import Note from '$lib/components/Note.svelte';
+	import { onMount } from 'svelte';
+	import { bb, areaSpline } from 'billboard.js';
+	import 'billboard.js/dist/theme/insight.css';
 
 	export let data;
 
 	const { pageViews, mostViewed } = data;
 
-	const lineChart = {
-		labels: pageViews.map((view) => view.day),
-		datasets: [
-			{
-				values: pageViews.map((view) => view.count),
+	onMount(() => {
+		bb.generate({
+			bindto: '#chart',
+			data: {
+				type: areaSpline(),
+				x: 'day',
+				columns: [
+					['day', ...pageViews.map((pw: any) => pw.day)],
+					['count', ...pageViews.map((pw: any) => pw.count)],
+				],
 			},
-		],
-	};
-
-	const pieChart = {
-		labels: mostViewed.map((page) => page.page),
-		datasets: [
-			{
-				values: mostViewed.map((page) => page.count),
+			axis: {
+				x: {
+					type: 'timeseries',
+					tick: {
+						format: '%Y-%m-%d',
+						rotate: 50,
+					},
+				},
 			},
-		],
-	};
+			tooltip: {
+				show: false,
+			},
+			point: {
+				show: false,
+			},
+			legend: {
+				hide: true,
+			},
+		});
+	});
 </script>
 
-<div class="p-2 md:p-8 grid grid-cols-1 md:grid-cols-2">
-	<div>
-		<h1 class="text-lg">Page views</h1>
-		<div class="">
-			<Chart
-				data={lineChart}
-				type="line"
-				lineOptions={{ spline: 1, hideDots: 1, regionFill: 1 }}
-				axisOptions={{ yAxisMode: 'tick', xAxisMode: 'tick', xIsSeries: 1 }} />
-		</div>
+<Note>
+	<div class="pt-12">
+		<h1 class="text-lg pb-8">Page views</h1>
+		<div id="chart" />
 	</div>
-	<div>
-		<h1 class="text-lg pt-8 md:pt-0">Most viewed</h1>
-		<Chart data={pieChart} type="pie" maxSlices={8} />
-	</div>
-</div>
+</Note>
