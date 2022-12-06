069816f6b03c3ea1df20df00b1ad24b93c6b938b
Florian Marending <florian@marending.dev>
2022-12-06 21:13:45 +0100

    Make caption serif

--- src/lib/components/lines/Figure.svelte
+++ src/lib/components/lines/Figure.svelte
@@ -5,6 +5,6 @@
 <div class="w-fit mx-auto pb-6 pt-4">
 	<slot />
 	{#if caption}
-		<div class="pt-4 text-center text-gray-800 text-sm italic">{caption}</div>
+		<div class="pt-4 text-center text-gray-800 text-sm italic font-serif">{caption}</div>
 	{/if}
 </div>
