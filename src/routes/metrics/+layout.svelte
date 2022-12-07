de5ff0c1349739b0242c76d98966373e486ea912
Florian Marending <florian@marending.dev>
2022-12-07 22:45:00 +0100

    Add links to metrics subpages

--- src/routes/metrics/+layout.svelte
+++ src/routes/metrics/+layout.svelte
@@ -2,13 +2,13 @@
 	import Note from '$lib/components/Note.svelte';
 </script>
 
+<div class="flex flex-row gap-4 pr-6 justify-end">
+	<a href="/metrics/views" class="text-gray-600 text-sm">page views</a>
+	<a href="/metrics/visited" class="text-gray-600 text-sm">most visited</a>
+	<a href="/metrics/referers" class="text-gray-600 text-sm">referers</a>
+</div>
 <Note>
 	<div class="pt-12">
 		<slot />
-	</div>
-	<div class="flex flex-row gap-4">
-		<a href="/metrics/views" class="text-gray-800 text-sm">page views</a>
-		<a href="/metrics/visited" class="text-gray-800 text-sm">most visited</a>
-		<a href="/metrics/referers" class="text-gray-800 text-sm">referers</a>
 	</div>
 </Note>
