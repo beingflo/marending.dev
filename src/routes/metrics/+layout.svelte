0cd42eeb72cbcc9711bdf2bb763d21900bbb4d21
Florian Marending <florian@marending.dev>
2022-12-08 20:50:15 +0100

    Add underline for metrics subpages

--- src/routes/metrics/+layout.svelte
+++ src/routes/metrics/+layout.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import Note from '$lib/components/Note.svelte';
+	import { page } from '$app/stores';
 </script>
 
 <div class="flex flex-row gap-4 pr-6 justify-end">
-	<a href="/metrics/views" class="text-gray-600 text-sm">page views</a>
-	<a href="/metrics/visited" class="text-gray-600 text-sm">most visited</a>
-	<a href="/metrics/referers" class="text-gray-600 text-sm">referers</a>
+	<a
+		href="/metrics/views"
+		class="text-gray-600 text-sm {$page.url.pathname === '/metrics/views' && 'underline'}"
+		>page views</a>
+	<a
+		href="/metrics/visited"
+		class="text-gray-600 text-sm  {$page.url.pathname === '/metrics/visited' && 'underline'}"
+		>most visited</a>
+	<a
+		href="/metrics/referers"
+		class="text-gray-600 text-sm  {$page.url.pathname === '/metrics/referers' && 'underline'}"
+		>referers</a>
 </div>
 <Note>
 	<div class="pt-12">
