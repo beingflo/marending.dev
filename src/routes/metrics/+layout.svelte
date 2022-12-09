083207d0f68c39174fddae39bce67e3a969cab34
Florian Marending <florian@marending.dev>
2022-12-09 23:44:37 +0100

    Change metrics subpage nav text color

--- src/routes/metrics/+layout.svelte
+++ src/routes/metrics/+layout.svelte
@@ -4,18 +4,14 @@
 </script>
 
 <div class="flex flex-row gap-4 pr-6 justify-end">
-	<a
-		href="/metrics/views"
-		class="text-gray-600 text-sm {$page.url.pathname === '/metrics/views' && 'underline'}"
+	<a href="/metrics/views" class="text-sm {$page.url.pathname === '/metrics/views' && 'underline'}"
 		>page views</a>
 	<a
 		href="/metrics/visited"
-		class="text-gray-600 text-sm  {$page.url.pathname === '/metrics/visited' && 'underline'}"
-		>most visited</a>
+		class="text-sm  {$page.url.pathname === '/metrics/visited' && 'underline'}">most visited</a>
 	<a
 		href="/metrics/referers"
-		class="text-gray-600 text-sm  {$page.url.pathname === '/metrics/referers' && 'underline'}"
-		>referers</a>
+		class="text-sm  {$page.url.pathname === '/metrics/referers' && 'underline'}">referers</a>
 </div>
 <Note>
 	<div class="pt-12">
