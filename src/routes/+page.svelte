929c268f812717a65f8a43ccd8b8401d639f91d2
Florian Marending <florian@marending.dev>
2022-12-12 23:11:12 +0100

    Use Page component in /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script>
 	import NoteSummary from '$lib/components/NoteSummary.svelte';
+	import Page from '$lib/components/Page.svelte';
 </script>
 
-<div
-	class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-8 md:pt-24 gap-8 md:gap-12 px-4 md:px-12">
-	<NoteSummary
-		title="A study of lines"
-		summary="Some schmuck tries his hands at generative art"
-		date="03 December 2022"
-		link="/notes/lines" />
-	<NoteSummary
-		title="About this website"
-		summary="There are many like it, but this one is mine"
-		date="27 November 2022"
-		link="/notes/website" />
-</div>
+<Page>
+	<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-12">
+		<NoteSummary
+			title="A study of lines"
+			summary="Some schmuck tries his hands at generative art"
+			date="03 December 2022"
+			link="/notes/lines" />
+		<NoteSummary
+			title="About this website"
+			summary="There are many like it, but this one is mine"
+			date="27 November 2022"
+			link="/notes/website" />
+	</div>
+</Page>
