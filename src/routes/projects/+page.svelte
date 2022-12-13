e5f70e836e4c53191cd279022a4aeb145898295a
Florian Marending <florian@marending.dev>
2022-12-13 21:28:06 +0100

    Increase spacing between projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,20 +6,20 @@
 <Page>
 	<div class="grid grid-cols-1 md:grid-cols-3 gap-y-2 md:gap-y-24">
 		<A href="https://marending.dev">marending.dev</A>
-		<span class="col-span-2"
+		<span class="col-span-2 mb-8 md:mb-0"
 			>My personal website. Custom designed and built with Sveltekit. It features a metrics
 			dashboard that displays page views, referers and commonly accessed routes. Deployed on a small
 			VPS using Caddy as a reverse proxy.
 		</span>
 		<A href="https://rest.quest">rest.quest</A>
-		<span class="col-span-2">
+		<span class="col-span-2 mb-8 md:mb-0">
 			A minimal and opinionated todo web application. The user interface is designed to remove
 			friction when adding projects or tasks. The state is primarily stored locally in the browser.
 			Optionally, the user can supply object storage credentials to synchronize the state across
 			multiple browsers. The application is implemented using SolidJS to keep the bundle size lean.
 		</span>
 		<A href="https://write.fieldnotes.land">fieldnotes.land</A>
-		<span class="col-span-2 mb-12 md:mb-0">
+		<span class="col-span-2 mb-8 md:mb-0">
 			An end-to-end encrypted note taking application. The web application features a <em>
 				wysiwyg</em> markdown editor and advanced formatting including tables. Content is encrypted using
 			a private key that is only stored in the browser. By encoding the decryption key in the hash of
