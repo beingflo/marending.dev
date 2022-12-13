124581f1a8f2fbe7ce16253f3647b33080350d86
Florian Marending <florian@marending.dev>
2022-12-13 21:12:39 +0100

    Enable metrics collection on /projects page

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,7 +5,7 @@ export const load = (params: any) => {
 	if (
 		params.url.pathname.startsWith('/notes/') ||
 		params.url.pathname.startsWith('/metrics/') ||
-		['/', '/about'].indexOf(params.url.pathname) > -1
+		['/', '/about', '/projects'].indexOf(params.url.pathname) > -1
 	) {
 		addPageView(params.url.pathname, params.request.headers.get('referer'));
 	}
