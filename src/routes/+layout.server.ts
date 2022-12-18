f4867f22890906b3d4a902c3898580fb99d8780c
Florian Marending <florian@marending.dev>
2022-12-18 20:16:53 +0100

    Add header check before logging view

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,11 @@ import { addPageView } from '$lib/db';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const load = (params: any) => {
+	// For load testing without distorting the metrics
+	if (params.request.headers.get('no-track')) {
+		return;
+	}
+
 	if (
 		params.url.pathname.startsWith('/notes/') ||
 		params.url.pathname.startsWith('/metrics/') ||
