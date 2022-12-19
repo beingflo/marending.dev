dc2cbdad11c0a19cbec38a1cf426e78cd4e4b57f
Florian Marending <florian@marending.dev>
2022-12-19 21:14:57 +0100

    [/website] Remove unnecessary imports

--- src/routes/notes/website/+page.svelte
+++ src/routes/notes/website/+page.svelte
@@ -1,10 +1,8 @@
 <script>
 	import C from '$lib/components/C.svelte';
 	import Code from '$lib/components/Code.svelte';
-	import Details from '$lib/components/Details.svelte';
 	import H2 from '$lib/components/H2.svelte';
 	import H3 from '$lib/components/H3.svelte';
-	import Hint from '$lib/components/Hint.svelte';
 	import Note from '$lib/components/Note.svelte';
 	import P from '$lib/components/P.svelte';
 	import Title from '$lib/components/Title.svelte';
