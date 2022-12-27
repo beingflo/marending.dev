685cdcc1fedb2874150f679fba51291edb5d18da
Florian Marending <florian@marending.dev>
2022-12-27 22:54:16 +0100

    Remove /feedback api

--- src/routes/api/feedback/+server.ts
+++
@@ -1,9 +0,0 @@
-import { addFeedback } from '$lib/db';
-
-export async function POST({ request }: { request: Request }) {
-	const { email, content } = await request.json();
-
-	addFeedback(content, email);
-
-	return new Response(null, { status: 200 });
-}
