60725dd7a432eec0fac02f49f76c13ddd69d0f10
Florian Marending <florian@marending.dev>
2022-12-28 19:57:08 +0100

    Don't make title text-justify

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -2,5 +2,7 @@
 	export let subtitle: string;
 </script>
 
-<h1 class="text-4xl md:text-6xl md:mb-2 text-black dark:text-white font-extrabold"><slot /></h1>
+<h1 class="text-4xl md:text-6xl md:mb-2 text-black dark:text-white font-extrabold text-left">
+	<slot />
+</h1>
 <h2 class="text-lg md:text-xl mb-16 md:mb-24">{subtitle}</h2>
