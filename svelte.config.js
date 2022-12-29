0944cbfb4073616c84194f906a3bf48030e217e0
Florian Marending <florian@marending.dev>
2022-12-29 20:23:23 +0100

    Add fallback page and turn on precompress

--- svelte.config.js
+++ svelte.config.js
@@ -16,8 +16,8 @@ const config = {
       // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
-      fallback: null,
-      precompress: false,
+      fallback: 'index.html',
+      precompress: true,
       strict: true
     })
 	}
