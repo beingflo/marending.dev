b03f935561cc3f2cdc1f5962c7f1c09312dad5d5
Florian Marending <florian@marending.dev>
2023-01-08 16:52:28 +0100

    left-align text in subtitle

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -5,7 +5,7 @@
 <h1 class="text-4xl md:text-6xl md:mb-2 text-black dark:text-white font-extrabold text-left">
 	<slot />
 </h1>
-<h2 class="text-lg md:text-xl mb-16 md:mb-24">{subtitle}</h2>
+<h2 class="text-lg md:text-xl mb-16 md:mb-24 text-left">{subtitle}</h2>
 
 <svelte:head>
 	<meta name="description" content={subtitle} />
