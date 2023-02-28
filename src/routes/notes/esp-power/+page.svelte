cdd0cc390b2aa5dce9f46ec1ed91eecfb3ee4eb4
Florian Marending <florian@marending.dev>
2023-02-28 22:46:48 +0100

    [esp-power] Continue writing

--- src/routes/notes/esp-power/+page.svelte
+++ src/routes/notes/esp-power/+page.svelte
@@ -15,4 +15,20 @@
 <Note>
 	<Title subtitle="First forays into embedded devices"
 		>Measuring power consumption of an ESP32 development board</Title>
+	<P
+		>I like some nice greenery in the living room. I also like data. Naturally, I have to monitor
+		the CO2, temperature and humidity in the room as well as the soil moisture and incident light on
+		the plant.
+	</P>
+	<P
+		>Fine, maybe I don't <em>have</em> to, but it's fun anyway. To make it happen without a huge mess
+		of cables, I would like to set up a system where the sensors are battery operated and report their
+		measurements into my monitoring setup (prometheus as of now).</P>
+	<P
+		>Until now, I've used Raspberry Pis and similar devices as low power home servers. They are
+		great as fully fledged file or web servers. For this endeavor however, they are clearly too
+		power hungry. I'm envisioning months if not years of battery life, not just days. So I started
+		playing with the idea of venturing into embedded devices. For simplicity I chose a well trodden
+		path: ESP32.
+	</P>
 </Note>
