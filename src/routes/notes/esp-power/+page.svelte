4dfb1d5cb1054dfb5d0ee5bd9aabbd05c712b52c
Florian Marending <florian@marending.dev>
2023-03-03 21:06:46 +0100

    [esp-power] Remove unused imports

--- src/routes/notes/esp-power/+page.svelte
+++ src/routes/notes/esp-power/+page.svelte
@@ -1,15 +1,8 @@
 <script>
-	import A from '$lib/components/A.svelte';
-	import C from '$lib/components/C.svelte';
-	import Code from '$lib/components/Code.svelte';
 	import H2 from '$lib/components/H2.svelte';
-	import H3 from '$lib/components/H3.svelte';
-	import Hint from '$lib/components/Hint.svelte';
 	import Note from '$lib/components/Note.svelte';
 	import P from '$lib/components/P.svelte';
 	import Title from '$lib/components/Title.svelte';
-	import { cpp } from 'svelte-highlight/languages';
-	import Details from '$lib/components/Details.svelte';
 	import Results from './results-table.svelte';
 	import Info from '$lib/components/Info.svelte';
 </script>
