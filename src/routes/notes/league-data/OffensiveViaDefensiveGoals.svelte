df1d7a68964722b8f3aceb3a0415e7f3b40ce8c7
Florian Marending <florian@marending.dev>
2023-05-17 22:41:59 +0200

    [league-data] Improve offensive vs defensive chart

--- src/routes/notes/league-data/OffensiveViaDefensiveGoals.svelte
+++ src/routes/notes/league-data/OffensiveViaDefensiveGoals.svelte
@@ -3,6 +3,8 @@
 	import * as Plot from '@observablehq/plot';
 	import type { Match } from './utils';
 	import Figure from '$lib/components/lines/Figure.svelte';
+	import A from '$lib/components/A.svelte';
+	import { d } from 'svelte-highlight/languages';
 
 	export let matches: Array<Match>;
 
@@ -13,7 +15,7 @@
 			if (players[p]) {
 				if (m.teamA[0] === p || m.teamB[0] === p) {
 					players[p].offensive += 1;
-				} else {
+				} else if (m.teamA[1] === p || m.teamB[1] === p) {
 					players[p].defensive += 1;
 				}
 				players[p].matches += 1;
@@ -23,26 +25,31 @@
 		});
 	});
 
-	const data = Object.entries(players).map(([player, { offensive, defensive, matches }]) => ({
+	let data = Object.entries(players).map(([player, { offensive, defensive, matches }]) => ({
 		player,
 		matches,
-		offensive,
-		defensive,
+		offensive: offensive / matches,
+		defensive: defensive / matches,
 	}));
+
+	data = data.filter((d) => d.matches > 10);
 </script>
 
 <Figure
-	caption="Figure 7. Goals scored from offensive vs defensive position for all players who played at least 100 matches.">
+	caption="Figure 7. Proportion of goals scored from offensive (circle) vs defensive (dot) position for all players who played at least 10 matches.">
 	<Chart
 		options={{
-			x: { grid: true },
-			y: { grid: true },
-			aspectRatio: 1,
+			y: { tickFormat: (d) => '' },
+			x: { grid: true, label: 'goal rate  →' },
 			marks: [
-				Plot.dot(
-					data.filter((d) => d.matches > 100),
-					{ x: 'offensive', y: 'defensive' }
-				),
+				Plot.link(data, {
+					x1: 'defensive',
+					x2: 'offensive',
+					markerStart: 'circle-stroke',
+					markerEnd: 'dot',
+					y: 'player',
+					sort: { y: 'x1' },
+				}),
 			],
 		}} />
 </Figure>
