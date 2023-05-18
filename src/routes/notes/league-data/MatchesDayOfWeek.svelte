1aa4561bf826477603205b62df1cc6da738e5e68
Florian Marending <florian@marending.dev>
2023-05-18 12:11:39 +0200

    [league-data] Simplify day of week

--- src/routes/notes/league-data/MatchesDayOfWeek.svelte
+++ src/routes/notes/league-data/MatchesDayOfWeek.svelte
@@ -5,23 +5,6 @@
 	import Figure from '$lib/components/lines/Figure.svelte';
 
 	export let matches: Array<Match>;
-
-	const days: Record<number, number> = {};
-
-	matches.forEach((m) => {
-		const day = m.startDate.getDay();
-		if (days[day]) {
-			days[day] += 1;
-		} else {
-			days[day] = 1;
-		}
-	});
-
-	const data = Object.entries(days).map(([day, matches]) => ({
-		day: Number(day),
-		matches,
-	}));
-	data.sort((a, b) => a.day - b.day);
 </script>
 
 <Figure caption="Figure 3. Day of week distribution of all games">
@@ -29,6 +12,6 @@
 		options={{
 			y: { grid: true, label: '↑ matches' },
 			x: { label: 'day', tickFormat: Plot.formatWeekday('en-US', 'short') },
-			marks: [Plot.barY(data, { x: 'day', y: 'matches' })],
+			marks: [Plot.barY(matches, Plot.groupX({ y: 'count' }, { x: (d) => d.startDate.getDay() }))],
 		}} />
 </Figure>
