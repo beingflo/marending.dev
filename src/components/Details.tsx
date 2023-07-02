a4efa74d3aba076d09d63d0ac60fe93014ce00f0
Florian Marending <florian@marending.dev>
2023-07-02 20:27:36 +0200

    fix box shadow on Details

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,20 +1,6 @@
-export const Details = ({
-  title,
-  children,
-}: {
-  title: string;
-  children: any;
-}) => (
-  <details class="mb-3 border border-black dark:border-white lg:-mx-4">
-    <summary class="cursor-pointer p-2">{title}</summary>
-    <div class="p-2 pt-0">{children}</div>
-  </details>
+export const Details = ({ title, children }: { title: string; children: any }) => (
+	<details class="mb-3 border border-black dark:border-white lg:-mx-4 shadow-[6px_6px_0_#00000020]">
+		<summary class="cursor-pointer p-2">{title}</summary>
+		<div class="p-2 pt-0">{children}</div>
+	</details>
 );
-
-{
-  /* <style>
-	details {
-		box-shadow: 6px 6px 0 #00000020;
-	}
-</style> */
-}
