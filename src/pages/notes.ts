f65f0acb596e1e7b5db552c4e8fbec313645c635
Florian Marending <florian@marending.dev>
2023-07-02 19:19:12 +0200

    consolidate title and subtitles

---
+++ src/pages/notes.ts
@@ -0,0 +1,38 @@
+export const notes = {
+	'co2-bedroom': {
+		title: 'The air quality impact of closing your bedroom door at night',
+		subtitle: 'Trying not to suffocate while sleeping',
+		date: '10 May 2023',
+		link: '/notes/co2-bedroom',
+	},
+	'esp-protocol': {
+		title: 'A comparison of wireless protocols for battery powered embedded devices',
+		subtitle: 'Trying to sleep as much as possible',
+		date: '17 April 2023',
+		link: '/notes/esp-protocol',
+	},
+	'esp-power': {
+		title: 'Measuring power consumption of ESP32 development boards',
+		subtitle: 'First forays into embedded devices',
+		date: '25 February 2023',
+		link: '/notes/esp-power',
+	},
+	performance: {
+		title: 'Performance tuning a specific SvelteKit application',
+		subtitle: `Making sure my website can handle all those page views it isn't getting`,
+		date: '29 December 2022',
+		link: '/notes/performance',
+	},
+	lines: {
+		title: 'A study of lines',
+		subtitle: 'Some schmuck tries his hands at generative art',
+		date: '03 December 2022',
+		link: '/notes/lines',
+	},
+	website: {
+		title: 'About this website',
+		subtitle: 'There are many like it, but this one is mine',
+		date: '27 November 2022',
+		link: '/notes/website',
+	},
+};
