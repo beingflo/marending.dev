bbf92090f05ea109e6ebece408a5573ab4fc398c
Florian Marending <florian@marending.dev>
2023-07-03 22:14:57 +0200

    Add ending slashes to links

--- src/pages/notes.ts
+++ src/pages/notes.ts
@@ -3,13 +3,13 @@ export const notes = {
 		title: 'The air quality impact of closing your bedroom door at night',
 		subtitle: 'Trying not to suffocate while sleeping',
 		date: '10 May 2023',
-		link: '/notes/co2-bedroom',
+		link: '/notes/co2-bedroom/',
 	},
 	'esp-protocol': {
 		title: 'A comparison of wireless protocols for battery powered embedded devices',
 		subtitle: 'Trying to sleep as much as possible',
 		date: '17 April 2023',
-		link: '/notes/esp-protocol',
+		link: '/notes/esp-protocol/',
 	},
 	'esp-power': {
 		title: 'Measuring power consumption of ESP32 development boards',
@@ -21,18 +21,18 @@ export const notes = {
 		title: 'Performance tuning a specific SvelteKit application',
 		subtitle: `Making sure my website can handle all those page views it isn't getting`,
 		date: '29 December 2022',
-		link: '/notes/performance',
+		link: '/notes/performance/',
 	},
 	lines: {
 		title: 'A study of lines',
 		subtitle: 'Some schmuck tries his hands at generative art',
 		date: '03 December 2022',
-		link: '/notes/lines',
+		link: '/notes/lines/',
 	},
 	website: {
 		title: 'About this website',
 		subtitle: 'There are many like it, but this one is mine',
 		date: '27 November 2022',
-		link: '/notes/website',
+		link: '/notes/website/',
 	},
 };
