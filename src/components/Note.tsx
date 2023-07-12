b6f1e1dfbbd1531418a66edae0b5477c180ba057
Florian Marending <florian@marending.dev>
2023-07-12 22:25:02 +0200

    No blocksatz

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -5,5 +5,5 @@ export type Props = {
 };
 
 export const Note = ({ children }: Props) => (
-	<article class="p-4 py-8 md:py-12 max-w-4xl mx-auto text-justify">{children}</article>
+	<article class="p-4 py-8 md:py-12 max-w-4xl mx-auto">{children}</article>
 );
