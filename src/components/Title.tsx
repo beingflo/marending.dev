996443001f1faaa31c18405cfc8b2accd3b1f59e
Florian Marending <florian@marending.dev>
2023-07-12 22:24:56 +0200

    Add more space below title on mobile

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -10,6 +10,6 @@ export const Title = ({ children, subtitle }: Props) => (
 		<h1 class="text-4xl md:text-6xl md:mb-2 text-black dark:text-white font-extrabold text-left">
 			{children}
 		</h1>
-		<h2 class="text-lg md:text-xl mb-16 md:mb-24 text-left">{subtitle}</h2>
+		<h2 class="text-lg md:text-xl mb-24 text-left">{subtitle}</h2>
 	</>
 );
