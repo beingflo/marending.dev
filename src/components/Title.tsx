d57ce110e0de2ab17f7b734b1f6cfe8116115ac0
Florian Marending <florian@marending.dev>
2024-03-02 19:23:24 +0100

    Add dates to note pages

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,11 +2,13 @@ import type { JSX } from 'solid-js/jsx-runtime';
 
 export type Props = {
 	subtitle: string;
+	date: string;
 	children: JSX.Element;
 };
 
-export const Title = ({ children, subtitle }: Props) => (
+export const Title = ({ children, subtitle, date }: Props) => (
 	<>
+		<p class="text-xs md:text-sm text-gray-600 dark:text-gray-200">{date}</p>
 		<h1 class="text-4xl md:text-6xl mb-4 text-black dark:text-white font-extrabold text-left">
 			{children}
 		</h1>
