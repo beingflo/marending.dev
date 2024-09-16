bebc0fd18f3077409724bb7311a19b523a6153e2
Florian Marending <florian@marending.dev>
2024-09-16 22:09:13 +0200

    Fix alignment of date in overview

--- src/components/NoteSummary.tsx
+++ src/components/NoteSummary.tsx
@@ -7,7 +7,7 @@ export type Props = {
 
 export const NoteSummary = ({ title, summary, date, link }: Props) => (
 	<a
-		class="flex flex-row md:hover:text-stone-600 md:dark:hover:text-stone-400"
+		class="flex flex-row items-baseline md:hover:text-stone-600 md:dark:hover:text-stone-400"
 		href={link}>
 		<p class="text-sm w-16 md:w-1/6">{date}</p>
 		<div class="flex flex-col xl:flex-row items-baseline md:gap-2 w-5/6">
