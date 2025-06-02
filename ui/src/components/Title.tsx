279403b10d993c8f44bce8b2f6cd60387e70b18f
Florian Marending <florian@marending.dev>
2025-06-02 21:54:36 +0200

    Start porting first note to mdx

--- ui/src/components/Title.tsx
+++ ui/src/components/Title.tsx
@@ -8,10 +8,10 @@ export type Props = {
 
 export const Title = ({ children, subtitle, date }: Props) => (
 	<>
-		<p class="text-xs md:text-sm text-gray-600 dark:text-gray-200">{date}</p>
-		<h1 class="text-4xl md:text-6xl mb-4 text-black dark:text-white font-extrabold text-left">
+		<p class="not-prose text-xs md:text-sm text-gray-600 dark:text-gray-200">{date}</p>
+		<h1 class="not-prose text-4xl md:text-6xl mb-4 text-black dark:text-white font-extrabold text-left">
 			{children}
 		</h1>
-		<h2 class="text-lg md:text-xl mb-24 text-left">{subtitle}</h2>
+		<h2 class="not-prose text-lg md:text-xl mb-24 text-left">{subtitle}</h2>
 	</>
 );
