6983055dd925cc6b6b39cf63cfa60dd2e71d4de9
Florian Marending <florian@marending.dev>
2025-06-03 21:42:00 +0200

    Migrate next note

--- ui/src/pages/notes.ts
+++ ui/src/pages/notes.ts
@@ -1,9 +1,1 @@
-export const notes = {
-	'16-hour-projects': {
-		title: '16 hour side-projects',
-		subtitle: 'An ode to the MVP',
-		date: '29 Feb 2024',
-		link: '/notes/16-hour-projects/',
-		draft: false,
-	},
-};
+export const notes = {};
