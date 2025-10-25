7a1a3e2e231d7565009c9956116c125e4a86eec4
Florian Marending <florian@marending.dev>
2025-10-25 23:17:52 +0200

    Make landing page 2 cols

--- ui/src/components/NoteSummary.tsx
+++ ui/src/components/NoteSummary.tsx
@@ -7,9 +7,9 @@ export type Props = {
 
 export const NoteSummary = ({ title, summary, date, link }: Props) => (
 	<a
-		class="flex flex-row items-baseline md:gap-8 w-full md:w-3/4 max-w-4xl mx-auto md:hover:text-stone-500 md:dark:hover:text-stone-400"
+		class="flex flex-row items-baseline md:gap-8 w-full md:hover:text-stone-500 md:dark:hover:text-stone-400"
 		href={link}>
-		<p class="text-sm w-16 md:w-24 font-light">{date}</p>
+		<p class="text-sm w-16 md:w-28 font-light">{date}</p>
 		<div class="flex flex-col w-full items-baseline">
 			<h3 class="font-semibold w-full underline">{title}</h3>
 			<p class="text-sm font-light">{summary}</p>
