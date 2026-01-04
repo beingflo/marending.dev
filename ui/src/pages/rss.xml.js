60f4551dd846af426ae8bcf777f1f6f474198267
Florian Marending <florian@marending.dev>
2026-01-04 22:27:44 +0100

    Add RSS feed

---
+++ ui/src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+	const blog = await getCollection('notes');
+
+	return rss({
+		title: 'Florian Marending',
+		description: 'Technical musings',
+		site: 'https://marending.dev',
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.subtitle,
+			// Compute RSS link from post `id`
+			// This example assumes all posts are rendered as `/blog/[id]` routes
+			link: `/blog/${post.id}/`,
+		})),
+	});
+}
