2935a96a866f16129fd5a9e81f152221966cb4e7
Florian Marending <florian@marending.dev>
2026-01-05 21:17:43 +0100

    Rename variable in rss

--- ui/src/pages/rss.xml.js
+++ ui/src/pages/rss.xml.js
@@ -1,14 +1,14 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-	const blog = await getCollection('notes');
+export async function GET() {
+	const notes = await getCollection('notes');
 
 	return rss({
 		title: 'Florian Marending',
 		description: 'Technical musings',
 		site: 'https://marending.dev',
-		items: blog.map((post) => ({
+		items: notes.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.date,
 			description: post.data.subtitle,
